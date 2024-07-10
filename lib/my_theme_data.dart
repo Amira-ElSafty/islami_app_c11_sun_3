89e5bcf6f0cc770e6aee4764a83ab6e30578c9f3
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2024-07-10 17:46:44 +0300

    Finished quran tab and quran details screen. Finished hadeth tab and hadeth details screen.

--- lib/my_theme_data.dart
+++ lib/my_theme_data.dart
@@ -8,14 +8,26 @@ class MyThemeData {
       primaryColor: AppColors.primaryLightColor,
       scaffoldBackgroundColor: Colors.transparent,
       appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.transparent, elevation: 0, centerTitle: true),
-      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-        selectedItemColor: AppColors.blackColor,
-      ),
-      textTheme: const TextTheme(
-        bodyLarge: TextStyle(
-            color: AppColors.blackColor,
-            fontSize: 30,
-            fontWeight: FontWeight.bold),
-      ));
+        iconTheme: IconThemeData(color: AppColors.blackColor),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      selectedItemColor: AppColors.blackColor,
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(
+          color: AppColors.blackColor,
+          fontSize: 30,
+          fontWeight: FontWeight.bold),
+      bodyMedium: TextStyle(
+          color: AppColors.blackColor,
+          fontSize: 25,
+          fontWeight: FontWeight.w700),
+      bodySmall: TextStyle(
+          color: AppColors.blackColor,
+          fontSize: 22,
+          fontWeight: FontWeight.bold),
+    ),
+  );
 }
